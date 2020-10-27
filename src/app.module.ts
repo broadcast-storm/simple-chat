e0ab99bb02b4505cd0391c41bd02e9a2cab00fce
Nikita Pozdnyakov <nikita220800@mail.ru>
2020-10-28 01:21:44 +0300

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+
+@Module({
+  imports: [UserModule, AuthModule, 
+    ServeStaticModule.forRoot({
+    rootPath: join(__dirname, '..', 'frontend', 'dist'),
+    exclude: ['/api*'],
+  }),
+  MongooseModule.forRoot('mongodb://localhost/simple-chat', { useNewUrlParser: true })
+],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
